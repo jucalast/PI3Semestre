c741e9d3cbdba739ebc9bd8d4d54af6bdd6a9bda
Gigio42 <gigio.42@gmail.com>
2024-08-25 23:51:45 -0300

    Modif back-end p usar lombok, tirei email coloquei senha e relação 1:1 (cliente:endereço). +Config para o front end rodar em vite.

--- Client/app/vite.config.js
+++ Client/app/vite.config.js
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: 'public/index.html'
+      '@': fileURLToPath(new URL('./src', import.meta.url)) // Ajuste do alias
     }
   }
-})
+});
