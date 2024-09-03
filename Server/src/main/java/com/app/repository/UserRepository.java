dfbf691da6cd23a9aa4ccb83456b58eaae84723b
Gigio42 <gigio.42@gmail.com>
2024-09-03 16:28:50 -0300

    Partes de Autenticação antes de documentar por completo.

--- Server/src/main/java/com/app/repository/UserRepository.java
+++ Server/src/main/java/com/app/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.app.repository;
 
-import com.app.model.Usuario;
+import com.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Usuario, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
