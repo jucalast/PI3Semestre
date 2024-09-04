867c70688ca4ed4d11c0c2877960e2c1c1964c6a
Gigio42 <gigio.42@gmail.com>
2024-09-04 19:31:15 -0300

    comentários e alteração de rotas permitidas

--- Server/src/main/java/com/app/DTO/RegisterRequest.java
+++ Server/src/main/java/com/app/DTO/RegisterRequest.java
@@ -14,8 +14,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    /**
+     * Primeiro nome do usuário.
+     */
     private String firstName;
+
+    /**
+     * Último nome do usuário.
+     */
     private String lastName;
+
+    /**
+     * E-mail do usuário.
+     */
     private String email;
+
+    /**
+     * Senha do usuário.
+     */
     private String password;
 }
