518196c9a5c9720dbe48ead663b6c5e95bf40a34
Gigio42 <gigio.42@gmail.com>
2024-09-11 15:37:56 -0300

    Atualizando p/ colocar banco de dados em nuvem.

--- Server/src/main/java/com/app/repository/CafeEspecialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seu_pacote.repository;
-
-import com.seu_pacote.model.CafeEspecial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CafeEspecialRepository extends JpaRepository<CafeEspecial, Long> {
-}
