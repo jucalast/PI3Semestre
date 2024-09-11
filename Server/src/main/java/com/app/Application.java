94d0ced416da347f5394d2480342fde7dc6cb0e7
Gigio42 <gigio.42@gmail.com>
2024-09-11 15:37:56 -0300

    Atualizando p/ colocar banco de dados em nuvem.

--- Server/src/main/java/com/app/Application.java
+++ Server/src/main/java/com/app/Application.java
@@ -1,5 +1,6 @@
 package com.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(Application.class, args);
     }
 }
