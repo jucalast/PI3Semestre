2d30a07193e5d8c85d03a4297b1f0f7b39424d9b
João Luccas <joaoluccas637@gmail.com>
2024-09-12 09:13:39 -0300

    trabalhando em alterações

---
+++ Server/src/main/java/com/app/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.app.service;
+
+import com.app.model.Product;
+import com.app.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
