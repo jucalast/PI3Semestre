1dfa9ffd7ce527bfc6843cece812fc3eef3b0485
joão Luccas <luccasmoura902@gmail.com>
2024-09-12 16:59:02 -0300

    construindo os primeirs componentes em vue e testando algumas rotas de produto

--- Server/src/main/java/com/app/repository/CafeEspecialRepository.java
+++ Server/src/main/java/com/app/repository/CafeEspecialRepository.java
@@ -1,11 +1,12 @@
-
 package com.app.repository;
 
-import com.app.model.CafeEspecial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
-@Repository
-public interface CafeEspecialRepository extends JpaRepository<CafeEspecial, Long> {
-    // Métodos personalizados, se necessário
+import com.app.model.CafeEspecial;
+
+import java.util.Set;
+
+public interface CafeEspecialRepository extends CrudRepository<CafeEspecial, Long> {
+    @SuppressWarnings("null")
+    Set<CafeEspecial> findAllById(Iterable<Long> ids);
 }
