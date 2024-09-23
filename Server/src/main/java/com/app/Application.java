479b323be6edbd0a40f4abc24a060795900b83fa
Gigio42 <gigio.42@gmail.com>
2024-09-23 19:46:46 -0300

    Resolvendo conflitos

--- Server/src/main/java/com/app/Application.java
+++ Server/src/main/java/com/app/Application.java
@@ -1,10 +1,13 @@
 package com.app;
 
+import com.app.config.RSAKeyRecord;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * Classe principal da aplicação Spring Boot.
@@ -15,6 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * configuração automática e
  * a detecção de componentes.
  */
+@EnableConfigurationProperties(RSAKeyRecord.class)
 @SpringBootApplication
 @EntityScan(basePackages = "com.app.model")
 @EnableJpaRepositories(basePackages = "com.app.repository")
@@ -25,6 +29,8 @@ public class Application {
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
 
         SpringApplication.run(Application.class, args);
     }
