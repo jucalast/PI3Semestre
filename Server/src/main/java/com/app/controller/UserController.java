f642993db7cce24189ff22a04f0169c07a89f457
Gigio42 <gigio.42@gmail.com>
2024-10-13 16:17:43 -0300

    arrumando background e fazendo redirect pra home após o login

--- Server/src/main/java/com/app/controller/UserController.java
+++ Server/src/main/java/com/app/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.app.controller;
 
 import com.app.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Controlador responsável pela gestão das rotas relacionadas ao usuário.
  *
  * Este controlador lida com a autenticação de usuários via OAuth2 e a exibição de perfis de usuário.
- *
+ * Ele também gerencia o redirecionamento para o frontend, utilizando uma URL configurável para diferentes ambientes.
+ * 
  * A anotação @Controller indica que esta classe é um componente do Spring que
  * trata as requisições web e retorna as respostas apropriadas.
- *
+ * 
  * @author Giovanni
  * @version 1.0
  * @since 2024-10-05
@@ -28,11 +29,16 @@ public class UserController {
     private final UserService userService;
 
     /**
-     * Construtor que injeta o serviço de usuário via autowired (injeção de dependência do Spring).
+     * URL do frontend, injetada a partir das configurações do aplicativo.
+     */
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    /**
+     * Construtor que injeta o serviço de usuário via injeção de dependência do Spring.
      *
      * @param userService O serviço de usuário a ser injetado.
      */
-    
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -69,11 +75,11 @@ public class UserController {
      * Rota que processa as informações do usuário após a autenticação com OAuth2.
      *
      * Este método salva o usuário no banco de dados se ele não existir e redireciona
-     * para o perfil do usuário.
+     * para a URL da home page.
      *
      * @param token O token de autenticação do usuário autenticado.
-     * @param model O modelo para passar dados à view.
-     * @return Um redirecionamento para a rota de perfil do usuário.
+     * @param model O modelo para passar dados à view (não utilizado neste método).
+     * @return Um redirecionamento para a URL do frontend.
      */
     @GetMapping("/process-user")
     public String processUser(OAuth2AuthenticationToken token, Model model) {
@@ -81,9 +87,6 @@ public class UserController {
         String email = token.getPrincipal().getAttribute("email");
 
         userService.saveUserIfNotExists(name, email);
-
-        model.addAttribute("name", name);
-        model.addAttribute("email", email);
-        return "redirect:/profile";
+        return "redirect:" + frontendUrl; 
     }
 }
