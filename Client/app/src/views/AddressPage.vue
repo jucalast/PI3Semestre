ba189c1c6561714fa0bad59bfe1f073ca2085bc5
Gigio42 <gigio.42@gmail.com>
2024-10-20 21:42:01 -0300

    pimpada no footer + minorfix

--- Client/app/src/views/AddressPage.vue
+++ Client/app/src/views/AddressPage.vue
@@ -1,7 +1,6 @@
 <template>
     <DefaultLayout>
         <div class="address-page p-6 bg-gray-100 min-h-screen flex flex-col">
-            <h1 class="text-3xl font-bold mb-6 text-gray-800">Gerenciar Endereços</h1>
 
             <form @submit.prevent="submitAddress" class="bg-white p-6 rounded-lg shadow-md mb-6 flex-grow">
                 <h2 class="text-2xl font-semibold mb-4">Adicionar/Editar Endereço</h2>
@@ -9,7 +8,14 @@
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div class="col-span-1">
                         <label class="block text-gray-700" for="zipCode">CEP</label>
-                        <input type="text" v-model="address.zipCode" id="zipCode" @blur="fetchAddressByZipCode" required class="input-cep" />
+                        <input type="text" v-model="address.zipCode" id="zipCode" @blur="fetchAddressByZipCode" required
+                            class="input-cep" />
+                        <p class="mt-1 text-sm text-gray-600">
+                            <a href="https://www.correios.com.br/para-voce/cep" target="_blank"
+                                class="text-blue-600 hover:underline">
+                                Procure seu CEP
+                            </a>
+                        </p>
                     </div>
 
                     <div>
@@ -24,7 +30,8 @@
 
                     <div>
                         <label class="block text-gray-700" for="neighborhood">Bairro</label>
-                        <input type="text" v-model="address.neighborhood" id="neighborhood" required class="input-field" />
+                        <input type="text" v-model="address.neighborhood" id="neighborhood" required
+                            class="input-field" />
                     </div>
 
                     <div>
@@ -40,7 +47,7 @@
 
                 <div class="flex mt-6">
                     <button type="submit"
-                        class="mr-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200">
+                        class="mr-4 px-4 py-2 bg-headerBackground text-white rounded-md hover:bg-yellow-600 transition duration-200">
                         {{ isEditing ? 'Atualizar Endereço' : 'Adicionar Endereço' }}
                     </button>
                     <button v-if="isEditing" type="button" @click="cancelEdit"
@@ -60,7 +67,8 @@
                     </div>
                     <div>
                         <button @click="editAddress(addr)" class="text-blue-600 hover:underline">Editar</button>
-                        <button @click="deleteAddress(addr.id)" class="ml-4 text-red-600 hover:underline">Remover</button>
+                        <button @click="deleteAddress(addr.id)"
+                            class="ml-4 text-red-600 hover:underline">Remover</button>
                     </div>
                 </li>
             </ul>
@@ -105,9 +113,11 @@ export default {
             }
         },
         async fetchAddressByZipCode() {
-            if (this.address.zipCode.length === 8) { 
+            const cleanZipCode = this.address.zipCode.replace(/\D/g, '');
+
+            if (cleanZipCode.length === 8) {
                 try {
-                    const response = await axios.get(`https://viacep.com.br/ws/${this.address.zipCode}/json/`);
+                    const response = await axios.get(`https://viacep.com.br/ws/${cleanZipCode}/json/`);
                     if (response.data && !response.data.erro) {
                         this.address.street = response.data.logradouro;
                         this.address.neighborhood = response.data.bairro;
@@ -181,7 +191,7 @@ export default {
 }
 
 .input-field:focus {
-    border-color: #D97706; 
+    border-color: #D97706;
     outline: none;
     box-shadow: 0 0 0 0.2rem rgba(255, 165, 0, 0.25);
 }
@@ -191,16 +201,16 @@ export default {
     display: block;
     width: 100%;
     height: 3rem;
-    border: 1px solid #D97706; 
+    border: 1px solid #D97706;
     border-radius: 0.375rem;
     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
     padding: 0.5rem;
-    background-color: #FFEDD5; 
+    background-color: #FFEDD5;
 }
 
 .input-cep:focus {
-    border-color: #B45309; 
+    border-color: #B45309;
     outline: none;
-    box-shadow: 0 0 0 0.2rem rgba(255, 140, 0, 0.25); 
+    box-shadow: 0 0 0 0.2rem rgba(255, 140, 0, 0.25);
 }
 </style>
